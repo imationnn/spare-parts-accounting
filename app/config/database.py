fd028894d57329f034135e37d6473ea9fea8bebf
roman <imationnn@yandex.ru>
2024-04-29 20:53:03 +0300

    refactoring

--- app/config/database.py
+++ app/config/database.py
@@ -10,15 +10,19 @@ class PGDatabase:
     def __init__(self, pg_config: PGConfig = PGConfig()):
         self.pg_config = pg_config
         self.engine = create_async_engine(url=self.pg_config.pg_dsn, echo=self.pg_config.echo)
-        self.session_factory = async_sessionmaker(
+        self.async_session_factory = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False
         )
 
+    @property
+    def session_factory(self):
+        return self.async_session_factory
+
     async def get_session(self) -> AsyncGenerator:
-        async with self.session_factory() as session:
+        async with self.async_session_factory() as session:
             yield session
 
 
