17088f6e7f7c025c88dbbe09f89be2367feadd3e
roman <imationnn@yandex.ru>
2024-06-02 10:09:35 +0300

    added client schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -32,3 +32,4 @@ from .actual_product_schema import (
     ActualProductUpdateIn,
     ActualProductUpdateOut
 )
+from .client_schema import PhysicalClientIn
---
+++ app/schemas/client_schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+
+class PhysicalClientIn(BaseModel):
+    first_name: str = Field(min_length=2, max_length=30)
+    last_name: str = Field(min_length=2, max_length=30)
+    patronymic: str | None = Field(default=None, min_length=2, max_length=30)
+    phone: str | None = Field(default=None, examples=["+7-900-111-11-11"])
+    email: str | None = Field(default=None)
+    sale: int | None = Field(default=None)
+    sale_card: str | None = Field(default=None)
+    comment: str | None = Field(default=None, max_length=300)
+    is_black_list: bool = Field(default=False)
