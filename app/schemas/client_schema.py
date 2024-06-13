b8672732943a95b6719260ff4b9e15a025198f85
roman <imationnn@yandex.ru>
2024-06-13 17:24:57 +0300

    refactoring

--- app/schemas/client_schema.py
+++ app/schemas/client_schema.py
@@ -1,16 +1,24 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+from fastapi import HTTPException
 
 
 class PhysicalClientIn(BaseModel):
-    first_name: str = Field(min_length=2, max_length=30)
-    last_name: str = Field(min_length=2, max_length=30)
-    patronymic: str | None = Field(default=None, min_length=2, max_length=30)
+    first_name: str = Field(min_length=1, max_length=30)
+    last_name: str = Field(min_length=1, max_length=30)
+    patronymic: str | None = Field(default=None, min_length=1, max_length=30)
     phone: str | None = Field(default=None, examples=["+7-900-111-11-11"])
     email: str | None = Field(default=None)
     sale: int = Field(default=0, ge=0)
     sale_card: str | None = Field(default=None)
     comment: str | None = Field(default=None, max_length=300)
     is_black_list: bool = Field(default=False)
+
+    @field_validator("first_name", "last_name", "patronymic", "phone", "email", "sale_card")
+    def strip_whitespaces(cls, field: str) -> str:
+        strip_field = field.strip()
+        if strip_field:
+            return strip_field
+        raise HTTPException(422, detail="String should have at least 1 characters")
 
 
 class PhysicalClientOut(PhysicalClientIn):
