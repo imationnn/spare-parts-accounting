58243fda99f8e4353b16caac01f0b39c8552d66a
roman <imationnn@yandex.ru>
2024-06-23 09:47:00 +0300

    added new client service method

--- app/services/client_service.py
+++ app/services/client_service.py
@@ -1,18 +1,22 @@
 from fastapi import Depends, HTTPException
 from sqlalchemy.exc import StatementError
 
-from app.repositories import PhysicalClientRepository, JuridicalClientRepository
-from app.schemas import PhysicalClientIn, PhysicalClientOut
+from app.repositories import PhysicalClientRepository, JuridicalClientRepository, OrganizationAttrRepository
+from app.schemas import PhysicalClientIn, PhysicalClientOut, JuridicalClientIn, JuridicalClientOut
 
 
 class ClientService:
     def __init__(
             self,
             physic_client_repository: PhysicalClientRepository = Depends(),
-            jur_client_repository: JuridicalClientRepository = Depends()
+            jur_client_repository: JuridicalClientRepository = Depends(),
+            org_attr_repository: OrganizationAttrRepository = Depends()
     ):
         self.physic_client_repository = physic_client_repository
         self.jur_client_repository = jur_client_repository
+        self.org_attr_repository = org_attr_repository
+
+    # For physic clients
 
     async def add_physic_client(self, client: PhysicalClientIn) -> PhysicalClientOut:
         try:
@@ -40,3 +44,20 @@ class ClientService:
             raise HTTPException(400, detail="Empty request")
         clients = await self.physic_client_repository.search_client(list_words)
         return [PhysicalClientOut.model_validate(client, from_attributes=True) for client in clients]
+
+    # For juridical clients
+
+    async def add_juridical_client(self, client: JuridicalClientIn) -> JuridicalClientOut:
+        attr_model = await self.org_attr_repository.add_organization_attrs(
+            **client.org_attr.model_dump(exclude_none=True)
+        )
+        try:
+            client_model = await self.jur_client_repository.add_new_client(
+                **client.model_dump(exclude_none=True, exclude={'org_attr'}),
+                org_attr_id=attr_model.id
+            )
+        except StatementError:
+            raise HTTPException(400, detail="This card number already added")
+        client_model.org_attr = attr_model
+        await self.jur_client_repository.session.commit()
+        return JuridicalClientOut.model_validate(client_model, from_attributes=True)
